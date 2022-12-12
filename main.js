111dfdd68d77ffd8f62aa565383f6477003e15f2
layerunk <89783661+layerunk@users.noreply.github.com>
2022-12-12 16:33:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,19 +4,20 @@ function preload(){
 
 function setup(){
     canvas = createCanvas(300 , 300);
-    canvas.position(500 , 100);
-    spy = createCapture(VIDEO);
-    spy.hide();
+    canvas.position(600 , 50);
+    sup = createCapture(VIDEO);
+    sup.hide();
     rainbow = "";
 }
 
 function draw(){
-    image(spy, 0, 0, 300, 300);
-    tint(rainbow);
+    image(sup, 0, 0, 300, 300);
+    circle(10, 300, 50);
+    circle(300, 300, 50);
+    circle(5, 2.5, 50);
+    circle(300, 5, 50);
 }
+
 function take_snapshot(){
-    save("IMG666.png");
-}
-function filter12(){
-    rainbow = document.getElementById("inputbox").value;
+    save("IMG999.png");
 }
